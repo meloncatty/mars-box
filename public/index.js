66d85cb5edcb811243b854f8844b8adf1660fc10
Krista Handel <k.handel66@gmail.com>
2018-07-16 10:49:50 -0600

    Append checkboxes and check if packed

--- public/index.js
+++ public/index.js
@@ -12,7 +12,15 @@ function getItems() {
       .then(items => {
         items.forEach(item => {
           let li = document.createElement('li')
+          let checkbox = document.createElement('input')
+          checkbox.type = "checkbox"
           itemList.append(item.item, li)
+          const grabListItems = document.querySelectorAll('li')
+          grabListItems.forEach(item => item.append(checkbox))
+          if (item.is_packed) {
+            checkbox.classList = 'packed'
+            checkbox.checked = true
+          }
         })
       })
   } catch(error) {
