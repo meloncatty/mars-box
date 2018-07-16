244a47b482c012ec0331db6b6d7867917ab7b37d
Krista Handel <k.handel66@gmail.com>
2018-07-16 10:00:05 -0600

    Setup GET req for items

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 
-app.set('port', process.env.PORT || 3000)
-
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
+
+app.set('port', process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
   res.send('Connected to server!')
+})
+
+app.get('/api/v1/essentials', (req, res) => {
+  database('mars-essentials').select()
+    .then(items => {
+      res.status(200).json(items)
+    })
+    .catch(error => {
+      res.status(400).send(`Error: ${error}`)
+    })
 })
 
 app.listen(app.get('port'), () => {
