777d0e4f9aeb3ba267c5783c2cbb6c63fb6d7a52
Krista Handel <k.handel66@gmail.com>
2018-07-16 11:17:53 -0600

    Add delete button functionality

--- public/index.js
+++ public/index.js
@@ -13,10 +13,17 @@ function getItems() {
         items.forEach(item => {
           let li = document.createElement('li')
           let checkbox = document.createElement('input')
+          let deleteButton = document.createElement('button')
+          deleteButton.addEventListener('click', deleteItem)
+          deleteButton.textContent = 'Delete'
           checkbox.type = "checkbox"
+          checkbox.value = item.id
           itemList.append(item.item, li)
           const grabListItems = document.querySelectorAll('li')
-          grabListItems.forEach(item => item.append(checkbox))
+          grabListItems.forEach(item => {
+            item.append(checkbox)
+            item.append(deleteButton)
+          })
           if (item.is_packed) {
             checkbox.classList = 'packed'
             checkbox.checked = true
@@ -26,6 +33,21 @@ function getItems() {
   } catch(error) {
     console.log(error)
   }
+}
+
+function deleteItem() {
+  const itemId = parseInt(this.previousSibling.value)
+  const url = `http://localhost:3000/api/v1/essentials/${itemId}`
+
+  try {
+    fetch(url, {
+      method: 'DELETE'
+    })
+      .then(res => console.log(res))
+  } catch(error) {
+    console.log(error)
+  }
+  // appInit()
 }
 
 appInit()
